91cd0461d059e92e294d0ec72805ddb1eea8dd12
Sk192011s <soekyawwin68@gmail.com>
2025-11-15 23:34:06 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 // main.ts
 import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
-import { connect } from "https://raw.githubusercontent.com/zizifn/edgetunnel/main/deno/connect.ts";
+
+// (NEW FIX) Using a different, active repository
+import { connect } in "https://raw.githubusercontent.com/3Kmfi6HP/EDtunnel/main/deno/connect.ts";
 
 const UUID = Deno.env.get("UUID") || "dba99842-a33e-4bd3-a183-26e4a690be03";
 
