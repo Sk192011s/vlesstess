9c8342e2f142e91b8fdcb07d420af88499e2141a
Sk192011s <soekyawwin68@gmail.com>
2025-11-15 23:29:44 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-// main.ts
+// main.ts (Fixed Import Link)
 import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
-import { connect } from "https://deno.land/x/vless@v1.0.0/mod.ts";
 
+// (FIX) This is the new, working import link
+import { connect } from "https://raw.githubusercontent.com/zizifn/edgetunnel/main/deno/connect.ts";
+
+// Deno Deploy ရဲ့ Environment Variable ကနေ UUID ကို ယူပါ
 const UUID = Deno.env.get("UUID") || "dba99842-a33e-4bd3-a183-26e4a690be03";
 
 serve((req: Request) => {
